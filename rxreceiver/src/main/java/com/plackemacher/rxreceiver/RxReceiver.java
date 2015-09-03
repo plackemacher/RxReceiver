d3b24a69259e76f3abe193555a1f93a302d6c508
Pat Lackemacher <patrick@lackemacher.com>
2015-09-03 05:22:31 -0400

    Cleanup formatting of code in RxReceiver

--- rxreceiver/src/main/java/com/plackemacher/rxreceiver/RxReceiver.java
+++ rxreceiver/src/main/java/com/plackemacher/rxreceiver/RxReceiver.java
@@ -2,9 +2,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,21 +30,21 @@ public final class RxReceiver {
      *
      * @param filter Selects the Intent broadcasts to be received.
      */
-    public static Observable<Intent> fromBroadcast(Context context, IntentFilter filter){
+    public static Observable<Intent> fromBroadcast(Context context, IntentFilter filter) {
         return Observable.create(new OnSubscribeBroadcastRegister(context, filter, null, null));
     }
 
     /**
      * Create Observable that wraps BroadcastReceiver and emits received intents.
      *
-     * @param filter Selects the Intent broadcasts to be received.
+     * @param filter              Selects the Intent broadcasts to be received.
      * @param broadcastPermission String naming a permissions that a
-     *      broadcaster must hold in order to send an Intent to you.  If null,
-     *      no permission is required.
-     * @param schedulerHandler Handler identifying the thread that will receive
-     *      the Intent.  If null, the main thread of the process will be used.
+     *                            broadcaster must hold in order to send an Intent to you.  If null,
+     *                            no permission is required.
+     * @param schedulerHandler    Handler identifying the thread that will receive
+     *                            the Intent.  If null, the main thread of the process will be used.
      */
-    public static Observable<Intent> fromBroadcast(Context context, IntentFilter filter, String broadcastPermission, Handler schedulerHandler){
+    public static Observable<Intent> fromBroadcast(Context context, IntentFilter filter, String broadcastPermission, Handler schedulerHandler) {
         return Observable.create(new OnSubscribeBroadcastRegister(context, filter, broadcastPermission, schedulerHandler));
     }
 
@@ -54,7 +54,7 @@ public final class RxReceiver {
      *
      * @param filter Selects the Intent broadcasts to be received.
      */
-    public static Observable<Intent> fromLocalBroadcast(Context context, IntentFilter filter){
+    public static Observable<Intent> fromLocalBroadcast(Context context, IntentFilter filter) {
         return Observable.create(new OnSubscribeLocalBroadcastRegister(context, filter));
     }
 }
